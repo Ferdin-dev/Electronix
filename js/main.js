c2a89b9ccb554e37da1e92e1458c23d416181935
ferdin <ndoloferdin@gmail.com>
2020-11-18 11:38:29 +0100

    Initial All files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  $('.main-header-info').owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    smartSpeed: 1500,
+    autoplay: true,
+    autoplaySpeed: 1000
+  })
+});
